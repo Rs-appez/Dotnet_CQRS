6873739a1bd13a2ffd03165e628af8270e9185ca
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-27 16:13:23 +0100

    feat(mongo): add init script and set default database
    Added a MongoDB initialization script to seed the 'film' collection with
    sample data. Updated docker compose to mount the script and set the
    default database to 'filmDB' for the MongoDB service.

---
+++ db/init.js
@@ -0,0 +1,20 @@
+db.film.insertMany([
+    {
+        Film_Id: 1,
+        Fr_Title: "Cars",
+        Qc_Title: "Les Bagnoles",
+        Release_Year: 2006,
+    },
+    {
+        Film_Id: 2,
+        Fr_Title: "Delire Express",
+        Qc_Title: "Ananas Express",
+        Release_Year: 2008,
+    },
+    {
+        Film_Id: 3,
+        Fr_Title: "The Dark Knight",
+        Qc_Title: "Le Chevalier Noir",
+        Release_Year: 2008,
+    },
+]);
