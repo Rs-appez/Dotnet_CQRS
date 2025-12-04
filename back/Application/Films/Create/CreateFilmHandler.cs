12a78f82f080a0351847e98c3e1178b09b0f4d8b
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-12-04 09:09:21 +0100

    feat(films): publish event after film creation
    Add IMessageBus dependency to CreateFilmHandler and publish a FilmDto event
    with routing key "order.created" after successfully adding a film. This
    enables other services to react to new film creations.

--- back/Application/Films/Create/CreateFilmHandler.cs
+++ back/Application/Films/Create/CreateFilmHandler.cs
@@ -3,9 +3,10 @@ using Domain.Entities;
 using MediatR;
 namespace Application.Films.Create;
 
-public class CreateFilmHandler(IFilmWriteRepository filmRepository) : IRequestHandler<CreateFilmCommand, int>
+public class CreateFilmHandler(IFilmWriteRepository filmRepository, IMessageBus messageBus) : IRequestHandler<CreateFilmCommand, int>
 {
     private readonly IFilmWriteRepository _filmRepository = filmRepository;
+    private readonly IMessageBus _messageBus = messageBus;
 
     public async Task<int> Handle(CreateFilmCommand command, CancellationToken cancellationToken)
     {
@@ -17,6 +18,9 @@ public class CreateFilmHandler(IFilmWriteRepository filmRepository) : IRequestHa
         );
 
         await _filmRepository.AddFilm(film, cancellationToken);
+        await _messageBus.PublishAsync(new FilmDto(film.Film_Id, film.Fr_Title, film.Qc_Title, film.Release_Year),
+                routingKey: "order.created",
+                cancellationToken);
         return film.Film_Id;
     }
 }
